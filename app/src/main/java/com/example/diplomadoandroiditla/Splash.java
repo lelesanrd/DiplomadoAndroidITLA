c024fe4b9420653e86892dec15660b75dbb563f0
Leandro Leonardo <leandroleonardo909@gmail.com>
2019-05-23 00:30:13 -0400

    creacion splash

--- app/src/main/java/com/example/diplomadoandroiditla/Splash.java
+++ app/src/main/java/com/example/diplomadoandroiditla/Splash.java
@@ -1,7 +1,13 @@
 package com.example.diplomadoandroiditla;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Splash extends AppCompatActivity {
 
@@ -9,5 +15,62 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
+        // La actividad está subiendo.
+
+        TextView tv = findViewById(R.id.tvInfoSplash);
+        ImageView iv = findViewById(R.id.ivLogo);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.splash_transition);
+        tv.startAnimation(myanim);
+        iv.startAnimation(myanim);
+        final Intent i = new Intent(this, MainActivity.class);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
+        // La actividad está a punto de hacerse visible.
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
+        // La actividad se ha vuelto visible (ahora se "reanuda").
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show();
+        // Enfocarse en otra actividad  (esta actividad está a punto de ser "detenida").
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
+        // La actividad ya no es visible (ahora está "detenida")
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
+        // La actividad está a punto de ser destruida.
     }
 }
