30230f88f738823e28bef7d6d9b1c6b32a031de5
Leandro Leonardo <leandroleonardo909@gmail.com>
2019-05-23 01:25:44 -0400

    creacion splash

--- app/src/main/java/com/example/diplomadoandroiditla/Splash.java
+++ app/src/main/java/com/example/diplomadoandroiditla/Splash.java
@@ -23,7 +23,7 @@ public class Splash extends AppCompatActivity {
         Animation myanim = AnimationUtils.loadAnimation(this, R.anim.splash_transition);
         tv.startAnimation(myanim);
         iv.startAnimation(myanim);
-        final Intent i = new Intent(this, MainActivity.class);
+        final Intent i = new Intent(this, MenuActivity.class);
         Thread timer = new Thread() {
             public void run() {
                 try {
